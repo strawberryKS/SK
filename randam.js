1691bec52911718ac83ecb61f51ad4d30eb50132
KS <106144986+strawberryKS@users.noreply.github.com>
2022-12-08 15:22:19 +0900

    Update randam.js

--- randam.js
+++ randam.js
@@ -1,5 +1,5 @@
 function getRandomInt(max){
   return Math.floor(Math.random() * max);
 }
-var links = ['https://forms.gle/YCSHifmA5SMyUraQA'];
+var links = ['https://forms.gle/UENUfwWG3qA6AmKn8'];
 window.location.href = links[getRandomInt(links.length)];
